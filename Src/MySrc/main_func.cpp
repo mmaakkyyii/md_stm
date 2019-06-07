2e98eda9f68749685efde25b00501f42c54ee2bd
mmaakkyyii <kyoro3141592@gmail.com>
2019-06-08 06:42:41 +0900

    指令値0が来たときの処理

--- Src/MySrc/main_func.cpp
+++ Src/MySrc/main_func.cpp
@@ -15,7 +15,7 @@ void Debug(char* data,int size){
 }
 
 
-uint8_t spi_buff[3]={0,0,0xf0};
+uint8_t spi_buff[3]={0,0,0xf3};
 int spi_data_size=3;
 
 void Init(){
@@ -49,7 +49,8 @@ void TimerInterrupt(){//10msおきに呼ばれる
 	spi[0]=spi_buff[0];
 	spi[1]=spi_buff[1];
 	spi[2]=spi_buff[2];
-	if(spi[0]==spi[1] && spi[1]==spi[2])spi[2]=1;
+	if(spi[0]==0 && spi[1]==0 && spi[2]==0)spi[2]=1;//配列がすべて0のときがあるけどこれをそのまま通すと回ってる最中に目標値0が入ってガタっとするので目標値0を送るときだけ特殊な値を送るようにしている
+	if(spi[0]==0 && spi[1]==0 && spi[2]==0xf0)spi[2]=0;//意図的に目標値0をｋ送るとき最後のビットは0xf0にすることにしているのでこんな感じ
 
 	int vel=(int16_t)((((uint16_t)spi[0])<<8) |((uint16_t)spi[1]));
 	int rx_state=spi[2];
@@ -59,7 +60,7 @@ void TimerInterrupt(){//10msおきに呼ばれる
 		pre_data=vel;
 		HAL_GPIO_WritePin(LED1_GPIO_Port,LED1_Pin,GPIO_PIN_SET);
 		spi[2]=0xaa;
-	}else if(spi[2]==0xf0){
+	}else if(spi[2]==0xf3){
 		HAL_GPIO_WritePin(LED1_GPIO_Port,LED1_Pin,GPIO_PIN_RESET);
 	}else{
 		vel=pre_data;
