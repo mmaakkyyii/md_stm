d965d671f67047bf0d80593c84356411cf0ef567
mmaakkyyii <kyoro3141592@gmail.com>
2019-06-14 22:32:19 +0900

    制御パラメータへんこう

--- Src/MySrc/my_variable.cpp
+++ Src/MySrc/my_variable.cpp
@@ -2,7 +2,7 @@
 
 Encoder encoder(1, 1024*4, 10, 30);
 Motor motor(1);
-ControllerZPID controller(8.08f, 56.3f, 0.0f, 0.01f, 10.0f);
+ControllerZPID controller(8.08f, 36.3f, 0.0f, 0.01f, 10.0f);
 
 
 
